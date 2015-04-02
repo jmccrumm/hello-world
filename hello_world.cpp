9eee2e9ae0462db3cdc76ad559e6bf41755198fc
jmccrumm <jmccrummen17@gmail.com>
2015-04-01 22:16:48 -0600

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+/*
+* Let's see what this github thing is all about...
+*
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello, GitHub!" << endl;
+  return 0;
+} // end main
